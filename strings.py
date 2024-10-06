9a70da44226b5e7c93cbb5c446e3a9de3026357c
Adeel <adeel@Adeels-MBP.fritz.box>
2024-10-06 05:12:09 +0200

    strings slicing

---
+++ strings.py
@@ -0,0 +1,17 @@
+name = 'Adeel'
+
+print (name)
+
+print (len(name))
+
+print(name[0:])
+
+print(name[1:])
+
+print(name[-1:]) #reverse slices index always start with -1 instead of 0 start point
+
+print(name[-10000:]) #what if wrong index is given
+
+print(name[:9888]) #what if wrong index is given
+
+print(name[555555:9885888888]) #what if wrong index is given . Nothing will be printed
