aa0947e492a3ffef5c7d636844e17fc08e285189
Adeel <adeel@Adeels-MBP.fritz.box>
2024-10-19 05:47:01 +0200

    tuples

---
+++ list and tuples.py
@@ -0,0 +1,13 @@
+any_list = ['Apples', 56, False, True, 156.89]
+
+print(any_list[0]) # print an element of array
+
+
+any_list[1] = 96
+
+print(any_list) #lists are mutable opposite to string which are immutable
+
+any_tuple = ('Apples', 56, False, True, 156.89)
+any_tuple1 = (1,3) #it will be tuple and not a single integer.
+
+print(any_tuple1[1])
